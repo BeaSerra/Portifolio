4bc3534dd7cd17dab34389628d5bcd10ca4f74a4
Beatriz Serra <129546519+BeaSerra@users.noreply.github.com>
2025-01-18 20:24:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-window.onload = function() {
-    alert("Bem-vindo ao meu portfólio!");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("show");
+  });
+});
+
